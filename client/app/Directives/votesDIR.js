e8f825b5119becd2deaa818563db6ce083b9b1f7
Kamron Batman <kamron.batman@gmail.com>
2015-09-16 19:52:48 -0700

    [Fix] Fixes voting and allows users to toggle their vote on/off by sending the same vote value

--- client/app/Directives/votesDIR.js
+++ client/app/Directives/votesDIR.js
@@ -1,35 +1,30 @@
 angular.module('fiddio')
-  .directive('votes', ['$http', '$rootScope', function($http, $rootScope) {
+  .directive('votes', ['$http', '$rootScope', '$timeout', function($http, $rootScope, $timeout) {
     return {
       restrict: 'E',
-      template: '<div><span ng-if="userData.authenticated"><i class="fa fa-chevron-circle-up" ng-click="vote(1)"></i> <i class="fa fa-chevron-circle-down" ng-click="vote(-1)"></i></span></div>',
+      templateUrl: '/templates/votes.html',
       replace: true,
       scope: {
-        responseId: '@'
+        answer: '='
       },
       link: function(scope, elm, attr) {
-        // fetch current vote from server and store it on $scope
-        $http({ method: 'GET', url: '/api/response/' + attr.responseId + '/vote' })
-        .then(function(response){
-          scope.currentVote = response.data.vote;
-        },function(response){});
-
         scope.userData = $rootScope.userData;
 
         // set up function to up- and downvote
         scope.vote = function(value) {
           var newVote = value === scope.currentVote ? 0 : value;
-          $http({ method: 'POST',
-            url: '/api/response/' + attr.responseId + '/vote',
-            data: { vote: newVote } })
-          .then(function(response){
-            scope.currentVote = newVote;
-            scope.updateStyle();
-          }, function(response){});
-        };
 
-        scope.updateStyle = function() {
-          //Update style based on scope.currentVote
+          $timeout( function() {
+            $http({ method: 'POST',
+            url: '/api/response/' + scope.answer.id + '/vote',
+            data: { vote: newVote } })
+            .then(function(response) {
+              if (response.data.result) {
+                console.log('response.data', response.data);
+                scope.answer.vote_count = response.data.vote_count;
+              }
+            });
+          });
         };
       }
     };
